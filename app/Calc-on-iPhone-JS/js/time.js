cceef429934622e20435ba4dc754b01b84260ddb
Andrushaa <andrew.ivanow505@gmail.com>
2021-05-08 13:15:48 +0300

    fix time in CalcJS

--- app/Calc-on-iPhone-JS/js/time.js
+++ app/Calc-on-iPhone-JS/js/time.js
@@ -4,11 +4,11 @@ const minute = document.querySelector('.minute');
 const UpdateTime = () => {
     const CurrentTime = new Date();
 
-    let CurrentHour = CurrentTime.getHours();
+    const CurrentHour = CurrentTime.getHours();
     const CurrentMinute = CurrentTime.getMinutes();
 
     hour.textContent = CurrentHour.toString();
-    minute.textContent = CurrentMinute.toString();
+    minute.textContent = CurrentMinute.toString().padStart(2, '0');
 }
 setInterval(UpdateTime, 1000);
 UpdateTime();
