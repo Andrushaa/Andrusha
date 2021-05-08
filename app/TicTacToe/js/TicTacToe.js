b6ba3f78d888058ac2ad852cf63fc1fa160fdecf
Andrushaa <andrew.ivanow505@gmail.com>
2021-05-08 22:00:44 +0300

    add TicTacToe and several bugs fixed

---
+++ app/TicTacToe/js/TicTacToe.js
@@ -0,0 +1,127 @@
+// HTML элементы
+const statusDiv = document.querySelector('.status-TicTacToe');
+const newGameDiv = document.querySelector('.newGame-TicTacToe');
+const cellDivs = document.querySelectorAll('.game-cell-TicTacToe');
+
+// константы
+const xSymbol = '×';
+const oSymbol = '○';
+
+// переменные
+let gameIsLive = true;
+let xIsNext = true;
+
+
+// функции
+const letterToSymbol = (letter) => letter === 'x' ? xSymbol : oSymbol;
+
+const handleWin = (letter) => {
+    gameIsLive = false;
+    if (letter === 'x') {
+        statusDiv.innerHTML = `Выиграл ${letterToSymbol(letter)}`;
+    } else {
+        statusDiv.innerHTML = `<span>Выиграл ${letterToSymbol(letter)}</span>`;
+    }
+};
+
+const checkGameStatus = () => {
+    const topLeft = cellDivs[0].classList[1];
+    const topMiddle = cellDivs[1].classList[1];
+    const topRight = cellDivs[2].classList[1];
+    const middleLeft = cellDivs[3].classList[1];
+    const middleMiddle = cellDivs[4].classList[1];
+    const middleRight = cellDivs[5].classList[1];
+    const bottomLeft = cellDivs[6].classList[1];
+    const bottomMiddle = cellDivs[7].classList[1];
+    const bottomRight = cellDivs[8].classList[1];
+
+  // проверяем выигрыш
+    if (topLeft && topLeft === topMiddle && topLeft === topRight) {
+        handleWin(topLeft);
+        cellDivs[0].classList.add('won');
+        cellDivs[1].classList.add('won');
+        cellDivs[2].classList.add('won');
+    } else if (middleLeft && middleLeft === middleMiddle && middleLeft === middleRight) {
+        handleWin(middleLeft);
+        cellDivs[3].classList.add('won');
+        cellDivs[4].classList.add('won');
+        cellDivs[5].classList.add('won');
+    } else if (bottomLeft && bottomLeft === bottomMiddle && bottomLeft === bottomRight) {
+        handleWin(bottomLeft);
+        cellDivs[6].classList.add('won');
+        cellDivs[7].classList.add('won');
+        cellDivs[8].classList.add('won');
+    } else if (topLeft && topLeft === middleLeft && topLeft === bottomLeft) {
+        handleWin(topLeft);
+        cellDivs[0].classList.add('won');
+        cellDivs[3].classList.add('won');
+        cellDivs[6].classList.add('won');
+    } else if (topMiddle && topMiddle === middleMiddle && topMiddle === bottomMiddle) {
+        handleWin(topMiddle);
+        cellDivs[1].classList.add('won');
+        cellDivs[4].classList.add('won');
+        cellDivs[7].classList.add('won');
+    } else if (topRight && topRight === middleRight && topRight === bottomRight) {
+        handleWin(topRight);
+        cellDivs[2].classList.add('won');
+        cellDivs[5].classList.add('won');
+        cellDivs[8].classList.add('won');
+    } else if (topLeft && topLeft === middleMiddle && topLeft === bottomRight) {
+        handleWin(topLeft);
+        cellDivs[0].classList.add('won');
+        cellDivs[4].classList.add('won');
+        cellDivs[8].classList.add('won');
+    } else if (topRight && topRight === middleMiddle && topRight === bottomLeft) {
+        handleWin(topRight);
+        cellDivs[2].classList.add('won');
+        cellDivs[4].classList.add('won');
+        cellDivs[6].classList.add('won');
+    } else if (topLeft && topMiddle && topRight && middleLeft && middleMiddle && middleRight && bottomLeft && bottomMiddle && bottomRight) {
+        gameIsLive = false;
+    statusDiv.innerHTML = 'Ничья!';
+    } else {
+    xIsNext = !xIsNext;
+    if (xIsNext) {
+        statusDiv.innerHTML = `Ходит ${xSymbol} `;
+        } else {
+        statusDiv.innerHTML = `<span>Ходит ${oSymbol}</span>`;
+        }
+    }
+};
+
+
+// обработчики событий
+const handleReset = () => {
+    xIsNext = true;
+    statusDiv.innerHTML = `Ходит ${xSymbol}`;
+    for (const cellDiv of cellDivs) {
+        cellDiv.classList.remove('x');
+        cellDiv.classList.remove('o');
+        cellDiv.classList.remove('won');
+    }
+    gameIsLive = true;
+};
+
+const handleCellClick = (e) => {
+    const classList = e.target.classList;
+
+    if (!gameIsLive || classList[1] === 'x' || classList[1] === 'o') {
+        return;
+    }
+
+    if (xIsNext) {
+        classList.add('x');
+        checkGameStatus();
+    } else {
+        classList.add('o');
+        checkGameStatus();
+    }
+};
+
+
+
+newGameDiv.addEventListener('click', handleReset);
+
+for (const cellDiv of cellDivs) {
+    cellDiv.addEventListener('click', handleCellClick)
+}
