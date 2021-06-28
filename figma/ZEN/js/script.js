2cb9afa8a97a8e902fe7d739bf034dfefdbf0b5e
Andrushaa <andrew.ivanow505@gmail.com>
2021-06-28 23:43:25 +0300

    add company animation

---
+++ figma/ZEN/js/script.js
@@ -0,0 +1,90 @@
+var a=document.getElementById("Booking");
+ 
+function onMouseCompany1(){
+    a.src="../ZEN/img/company-pink/Booking.png";
+}
+ 
+function outMouseCompany1(){
+   a.src = "../ZEN/img/company/Booking.png";
+}
+
+
+var b=document.getElementById("AliExpress");
+ 
+function onMouseCompany2(){
+    b.src="../ZEN/img/company-pink/AliExpress.png";
+}
+ 
+function outMouseCompany2(){
+   b.src = "../ZEN/img/company/AliExpress.png";
+}
+
+var c=document.getElementById("Allegro");
+ 
+function onMouseCompany3(){
+    c.src="../ZEN/img/company-pink/Allegro.png";
+}
+ 
+function outMouseCompany3(){
+   c.src = "../ZEN/img/company/Allegro.png";
+}
+
+var d=document.getElementById("Zalando-lounge");
+ 
+function onMouseCompany4(){
+    d.src="../ZEN/img/company-pink/Zalando-lounge.png";
+}
+ 
+function outMouseCompany4(){
+   d.src = "../ZEN/img/company/Zalando-lounge.png";
+}
+
+var e=document.getElementById("Lidl");
+ 
+function onMouseCompany5(){
+    e.src="../ZEN/img/company-pink/Lidl.png";
+}
+ 
+function outMouseCompany5(){
+   e.src = "../ZEN/img/company/Lidl.png";
+}
+
+var f=document.getElementById("MadiaMarkt");
+ 
+function onMouseCompany6(){
+    f.src="../ZEN/img/company-pink/MadiaMarkt.png";
+}
+ 
+function outMouseCompany6(){
+   f.src = "../ZEN/img/company/MadiaMarkt.png";
+}
+
+var f=document.getElementById("MadiaMarkt");
+ 
+function onMouseCompany6(){
+    f.src="../ZEN/img/company-pink/MadiaMarkt.png";
+}
+ 
+function outMouseCompany6(){
+   f.src = "../ZEN/img/company/MadiaMarkt.png";
+}
+
+var g=document.getElementById("RTV-evro-AGD");
+ 
+function onMouseCompany7(){
+    g.src="../ZEN/img/company-pink/RTV-evro-AGD.png";
+}
+ 
+function outMouseCompany7(){
+   g.src = "../ZEN/img/company/RTV-evro-AGD.png";
+}
+
+var h=document.getElementById("Groupon");
+ 
+function onMouseCompany8(){
+    h.src="../ZEN/img/company-pink/Groupon.png";
+}
+ 
+function outMouseCompany8(){
+   h.src = "../ZEN/img/company/Groupon.png";
+}
