229fd4f2c4b8853245e519e2032e33ca99a411d4
Andrushaa <andrew.ivanow505@gmail.com>
2021-07-03 22:40:59 +0300

    add burger-menuZEN and any fix

--- figma/ZEN/js/burgerMenu.js
+++ figma/ZEN/js/burgerMenu.js
@@ -2,8 +2,8 @@ const iconMenu = document.querySelector('.menu-burgerZEN');
 const menuBody = document.querySelector('.menu-bodyZEN');
 if (iconMenu) {
 	iconMenu.addEventListener("click", function (e) {
-		document.body.classList.toggle('_lockZEN');
-		iconMenu.classList.toggle('_active');
-		menuBody.classList.toggle('_active');
+		document.body.classList.toggle('lockZEN');
+		iconMenu.classList.toggle('activeHeaderZEN');
+		menuBody.classList.toggle('activeHeaderZEN');
 	});
 }
