ee23ef4a210f2654bd80b5f78352325dfac795bb
Andrushaa <andrew.ivanow505@gmail.com>
2021-07-06 22:51:02 +0300

    add SOS copy

---
+++ figma/SOS-old/js/button-subscribe.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const e = React.createElement;
+
+class ButtonSubscribe extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { liked: false };
+    }
+
+    render() {
+        if (this.state.liked) {
+            return 'Confirm your e-mail, please';
+        }
+
+        return e(
+            'button',
+            { onClick: () => this.setState({ liked: true }) },
+            'Subscribe now'
+        );
+    }
+}
+
+const domContainer = document.querySelector('.ButtonSubscribe');
+ReactDOM.render(e(ButtonSubscribe), domContainer);
