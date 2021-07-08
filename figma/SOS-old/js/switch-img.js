6d002cb6d7cb7a229e6c9cdcbe5efd222691ede3
Andrushaa <andrew.ivanow505@gmail.com>
2021-07-08 22:31:33 +0300

    .

--- figma/SOS-old/js/switch-img.js
+++
@@ -1,8 +0,0 @@
-var i = 0;
-var image = document.getElementById("imgSwitch-1");
-var imgs = new Array('../SOS/img/screenshot-1.jpg', '../SOS/img/screenshot-2.jpg', '../SOS/img/screenshot-3.jpg');
-function imgSwitch1() {
-    i++;
-    i %= imgs.length;
-    image.src = imgs[i];
-}
