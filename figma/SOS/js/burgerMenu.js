7de663a7060911c4d2a51a782739eba3be37a856
Andrushaa <andrew.ivanow505@gmail.com>
2021-07-15 23:36:33 +0300

    fix burgerMenu SOS

--- figma/SOS/js/burgerMenu.js
+++ figma/SOS/js/burgerMenu.js
@@ -2,7 +2,7 @@ const iconMenu = document.querySelector('.menu-burgerSOS');
 const menuBody = document.querySelector('.menu-bodySOS');
 if (iconMenu) {
 	iconMenu.addEventListener("click", function (e) {
-		document.body.classList.toggle('lockSOS');
+		// document.body.classList.toggle('lockSOS');
 		iconMenu.classList.toggle('activeHeaderSOS');
 		menuBody.classList.toggle('activeHeaderSOS');
 	});
@@ -12,7 +12,6 @@ const listMenuSOS = document.querySelector('.menu-listSOS');
 
 if (listMenuSOS) {
 	listMenuSOS.addEventListener('click', function(a) {
-		document.body.classList.toggle('lockSOS');
 		iconMenu.classList.toggle('activeHeaderSOS');
 		menuBody.classList.toggle('activeHeaderSOS');
 	})
