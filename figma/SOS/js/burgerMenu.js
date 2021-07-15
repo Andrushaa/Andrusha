532fba338c00944f7f601e89f40bd89f5ccab2d1
Andrushaa <andrew.ivanow505@gmail.com>
2021-07-15 23:12:53 +0300

    fix SOS burger menu

--- figma/SOS/js/burgerMenu.js
+++ figma/SOS/js/burgerMenu.js
@@ -7,3 +7,13 @@ if (iconMenu) {
 		menuBody.classList.toggle('activeHeaderSOS');
 	});
 }
+
+const listMenuSOS = document.querySelector('.menu-listSOS');
+
+if (listMenuSOS) {
+	listMenuSOS.addEventListener('click', function(a) {
+		document.body.classList.toggle('lockSOS');
+		iconMenu.classList.toggle('activeHeaderSOS');
+		menuBody.classList.toggle('activeHeaderSOS');
+	})
+}
