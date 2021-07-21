a97009f2d154db2ffc06bbfe1a5d3738856f2fd7
Andrushaa <andrew.ivanow505@gmail.com>
2021-07-21 20:10:19 +0300

    scroll

--- figma/SOS/js/backgroundScroll.js
+++ figma/SOS/js/backgroundScroll.js
@@ -2,12 +2,29 @@ let backgrounds = document.getElementById("backgrounds");
 
 backgrounds.style.backgroundImage = 'url(../SOS/img/background/bg-image-1-min.jpg)';
 
+
 $(window).scroll(function() {
-    if ($(this).scrollTop() > 700) {
+    if ($(this).scrollTop() > 3800) {
+        backgrounds.style.backgroundImage = 'url(../SOS/img/background/bg-image-6-min.jpg)';
+    } else if ($(this).scrollTop() > 3200) {
+        backgrounds.style.backgroundImage = 'url(../SOS/img/background/bg-image-5-min.jpg)';
+    }   
+
+    else if ($(this).scrollTop() > 2400) {
+        backgrounds.style.backgroundImage = 'url(../SOS/img/background/bg-image-3-min.jpg)';
+    }
+    
+    else if ($(this).scrollTop() > 1600) {
+        backgrounds.style.backgroundImage = 'url(../SOS/img/background/bg-image-4-min.jpg)';
+
+    }
+
+    else if ($(this).scrollTop() > 700) {
         backgrounds.style.backgroundImage = 'url(../SOS/img/background/bg-image-2-min.jpg)';
-    } else {
+    }
+    
+    else {
         backgrounds.style.backgroundImage = 'url(../SOS/img/background/bg-image-1-min.jpg)';
     }
+
 });
-
-
