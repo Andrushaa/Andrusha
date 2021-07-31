bc44400dc866b47d9917c0b427909db0cd318d86
Andrushaa <andrew.ivanow505@gmail.com>
2021-07-31 21:42:49 +0300

    Meditation

---
+++ figma/Mediation/js/burgerMenu.js
@@ -0,0 +1,9 @@
+const iconMenu = document.querySelector('.Meditation-header__burgerMenu');
+const menuBody = document.querySelector('.Meditation-header__menu-body');
+if (iconMenu) {
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle('lockMeditation');
+		iconMenu.classList.toggle('activeMeditation-header');
+		menuBody.classList.toggle('activeMeditation-header');
+	});
+}
