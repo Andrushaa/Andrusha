921e624572f8a0c3e4c4bdbd9a7eeb1cdc5b50ee
Andrushaa <andrew.ivanow505@gmail.com>
2021-08-02 23:34:36 +0300

    Meditation

---
+++ figma/Mediation/js/Meditation-item.js
@@ -0,0 +1,15 @@
+const item = document.getElementById("Meditation-items");
+
+for (let element of item.children) {
+  const title = element.firstElementChild;
+
+  title.onmouseover = () => {
+    for (let block of item.children) {
+      if (block === element) {
+        block.firstElementChild.classList.add("Meditation-item__article-active");
+      } else {
+        block.firstElementChild.classList.remove("Meditation-item__article-active");
+      }
+    }
+  };
+}
