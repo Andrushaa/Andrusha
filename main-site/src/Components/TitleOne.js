748b476f847d00c4520cfb89ae803ff56ad2f4b9
Andrusha <andrew.ivanow505@gmail.com>
2021-08-29 22:06:48 +0300

    react add "person"

--- main-site/src/Components/TitleOne.js
+++ main-site/src/Components/TitleOne.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
+import './person.css';
+import hamster from "./img/humster.jpg"
 
 class TitleOne extends Component {
     render() {
         return (
-            <h1>Junior фронтенд разработчик</h1>
+            <>
+                <h1>Junior фронтенд разработчик</h1>
+                <div className="person">
+                    <div className="person-item__img">
+                        <img 
+                            src={hamster}
+                            height="360"
+                            width="360"
+                            alt="person"
+                        />
+                    </div>
+                    <div className="person-item__text">
+                        Ершов Андрей
+                    </div>
+                </div>
+            </>
         );
     }
 }
