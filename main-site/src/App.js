650bb75e90de723f4408ca897107498a262f1598
Andrusha <andrew.ivanow505@gmail.com>
2021-08-30 22:27:14 +0300

    react add education

--- main-site/src/App.js
+++ main-site/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import Person from "./Components/Person";
+import Person from "./Components/person/Person";
+import Education from "./Components/education/Education";
 
 function App() {
   return (
     <>
       <Person />
+      <Education />
     </>
   );
 }
---
+++ main-site/src/Components/education/Education.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import './education.css';
+
+class Education extends Component {
+    render() {
+        return (
+            <>
+                <h1>Образование</h1>
+                <div className="education">
+                    <div className="education-item">
+
+                        <i className="fas fa-graduation-cap"></i>
+                        <p>
+                            ТулГУ,
+                            <br/>
+                            Институт прикладной математики и компьютерных наук.
+                        </p>
+                        <p>
+                            кафедра: "Прикладная математика и информатика"
+                        </p>
+                        <p>
+                            Диплом бакалавра - средний балл 4,02
+                        </p>
+                        <p>
+                            Диплом магистра - средний балл 4,91
+                            <br/>
+                            (красный диплом)
+                        </p>
+                    </div>
+                    <div className="education-item">
+                        <i className="fas fa-graduation-cap"></i>
+                        <p>
+                            HTML
+                        </p>
+                        <p>
+                            CSS/SASS
+                        </p>
+                        <p>
+                            JavaScript
+                        </p>
+                        <p>
+                            React
+                        </p>
+                        <p>
+                            Selenium
+                        </p>
+                        <p>
+                            C#
+                        </p>
+                    </div>
+                </div>
+            </>
+        );
+    }
+}
+
+export default Education;
