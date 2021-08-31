d11b96a7c0d435901e50e3aaf29e874167388e11
Andrusha <andrew.ivanow505@gmail.com>
2021-08-31 19:29:28 +0300

    react add works

--- main-site/src/App.js
+++ main-site/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Person from "./Components/person/Person";
 import Education from "./Components/education/Education";
+import Works from "./Components/works/Works";
 
 function App() {
   return (
     <>
       <Person />
       <Education />
+      <Works />
     </>
   );
 }
---
+++ main-site/src/Components/works/Works.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './works.css';
+
+class Works extends Component {
+    render() {
+        return (
+            <>
+                <h1>Мои работы</h1>
+                
+            </>
+        );
+    }
+}
+
+export default Works;
