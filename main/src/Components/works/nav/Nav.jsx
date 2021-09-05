1d6fd935882478c54ec76e04eaa5a72f8a501a18
Andrusha <andrew.ivanow505@gmail.com>
2021-09-05 11:41:22 +0300

    react fix tabs

--- main/src/Components/works/nav/Nav.jsx
+++ main/src/Components/works/nav/Nav.jsx
@@ -1,17 +1,22 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {Link, BrowserRouter}  from 'react-router-dom';
+import Works from "./../Works";
 import './nav.css';
 
 class Nav extends Component {
     render() {
         return (
-            <div className="works-tabs">
-                <div className="works-tab">
-                    макеты
+            <Router>
+                <div className="works-tabs">
+                    <Link to="/layout"  className="works-tab">макеты</Link>
+                    <Link to="/apps"  className="works-tab">приложения</Link>
+                    <Route exact path="/" component={Works} />
+                    <Route path="/products" component={Works} />
                 </div>
-                <div className="works-tab">
-                    приложения
-                </div>
-            </div>
+            </Router>
+
         );
     }
 }
