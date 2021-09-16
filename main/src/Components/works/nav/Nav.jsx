9668ddaa7a8de4f2e5c0153cffcd212a5b9a4b04
Andrusha <andrew.ivanow505@gmail.com>
2021-09-16 22:05:28 +0300

    main: added functional components

--- main/src/Components/works/nav/Nav.jsx
+++ main/src/Components/works/nav/Nav.jsx
@@ -1,26 +1,24 @@
-import React, {Component} from 'react';
+import React  from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Works from "./works/Works";
 import Apps from "./apps/Apps";
 import './nav.css';
 
-class Nav extends Component {
-    render() {
-        return (
-            <Router>
-                <div className="works-tabs">
-                    <Link to="/"  className="works-tab">макеты</Link>
-                    <Link to="/apps"  className="works-tab">приложения</Link>
-                </div>
-                <Switch>
-                    <Route exact path="/" component={Works} />
-                    <Route exact path="/apps" component={Apps} />
-                </Switch>
+function Nav() {
+    return (
+        <Router>
+            <div className="works-tabs">
+                <Link to="/"  className="works-tab">макеты</Link>
+                <Link to="/apps"  className="works-tab">приложения</Link>
+            </div>
+            <Switch>
+                <Route exact path="/" component={Works} />
+                <Route exact path="/apps" component={Apps} />
+            </Switch>
 
-            </Router>
+        </Router>
 
-        );
-    }
+    );
 }
 
 export default Nav;
