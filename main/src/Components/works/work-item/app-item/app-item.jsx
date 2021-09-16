574e0111ece0ec72271419931d8d5df84eb733c3
Andrusha <andrew.ivanow505@gmail.com>
2021-09-16 22:03:29 +0300

    main: added functional components

--- main/src/Components/works/work-item/app-item/app-item.jsx
+++ main/src/Components/works/work-item/app-item/app-item.jsx
@@ -1,29 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './app-item.css';
 
-class AppItem extends Component {
-    render() {
-        return (
-            <div className="app">
-                <div className="app-img__wrapper">
-                    <div className="app-img">
-                        <img
-                            src={this.props.image}
-                            alt="logo"
-                        />
-                    </div>
+function AppItem(props) {
+    return (
+        <div className="app">
+            <div className="app-img__wrapper">
+                <div className="app-img">
+                    <img
+                        src={props.image}
+                        alt="logo"
+                    />
                 </div>
-
-                <div className="app-title">
-                    {this.props.title}
-                </div>
-                <div className="app-type">
-                    {this.props.descr}
-                </div>
-                <a href={this.props.watch} target="_blank" rel="noreferrer" className="app-watch">Смотреть</a>
             </div>
-        );
-    }
+
+            <div className="app-title">
+                {props.title}
+            </div>
+            <div className="app-type">
+                {props.descr}
+            </div>
+            <a href={props.watch} target="_blank" rel="noreferrer" className="app-watch">Смотреть</a>
+        </div>
+    );
 }
 
 export default AppItem;
--- main/src/Components/works/work-item/layout-item/layout-item.jsx
+++ main/src/Components/works/work-item/layout-item/layout-item.jsx
@@ -1,30 +1,28 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './layout-item.css';
 
-class LayoutItem extends Component {
-    render() {
-        return (
-            <div className="work">
-                <div className="work-img__wrapper">
-                    <div className="work-img">
-                        <img
-                            src={this.props.image}
-                            alt="logo"
-                        />
-                    </div>
+function LayoutItem(props) {
+    return (
+        <div className="work">
+            <div className="work-img__wrapper">
+                <div className="work-img">
+                    <img
+                        src={props.image}
+                        alt="logo"
+                    />
                 </div>
-
-                <div className="work-title">
-                    {this.props.title}
-                </div>
-                <div className="work-type">
-                    {this.props.descr}
-                </div>
-                <a href={this.props.layout} target="_blank" rel="noreferrer" className="work-layout">Макет</a>
-                <a href={this.props.watch} target="_blank" rel="noreferrer" className="work-watch">Смотреть</a>
             </div>
-        );
-    }
+
+            <div className="work-title">
+                {props.title}
+            </div>
+            <div className="work-type">
+                {props.descr}
+            </div>
+            <a href={props.layout} target="_blank" rel="noreferrer" className="work-layout">Макет</a>
+            <a href={props.watch} target="_blank" rel="noreferrer" className="work-watch">Смотреть</a>
+        </div>
+    );
 }
 
 export default LayoutItem;
