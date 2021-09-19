f5ce5baff40fcc04b46cd4f2b77b77a6bb418e73
Andrusha <andrew.ivanow505@gmail.com>
2021-09-19 19:30:05 +0300

    add propTypes in Title.jsx

--- main/src/Components/title/Title.jsx
+++ main/src/Components/title/Title.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './title.css';
 
-const Title = (props) => {
+const Title = props => {
     return (
         <h1>{props.titleName}</h1>
     );
+};
+
+Title.propTypes = {
+    titleName: PropTypes.string.isRequired
 };
 
 export default Title;
