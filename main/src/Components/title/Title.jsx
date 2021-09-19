db92c6b467d1b3e1a6e90473bb95828ec2c0a97b
Andrusha <andrew.ivanow505@gmail.com>
2021-09-19 20:39:29 +0300

    fix

--- main/src/Components/title/Title.jsx
+++ main/src/Components/title/Title.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './title.css';
 
-const Title = props => {
+function Title (props) {
     return (
         <h1>{props.titleName}</h1>
     );
-};
+}
 
 Title.propTypes = {
     titleName: PropTypes.string.isRequired
