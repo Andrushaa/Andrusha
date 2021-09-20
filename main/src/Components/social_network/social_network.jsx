2c47bdd61b2b72d8ab6177d443c896ddf6fc96a1
Andrusha <andrew.ivanow505@gmail.com>
2021-09-20 23:29:06 +0300

    add useState in social-network

--- main/src/Components/social_network/social_network.jsx
+++ main/src/Components/social_network/social_network.jsx
@@ -3,15 +3,16 @@ import './social_network.css';
 
 function SocialNetwork() {
     let [count, setCount] = useState("Социальные сети");
+    let network = "Социальные сети"
 
     return (
         <>
             <div className="social-network">
-                <button onMouseOver={() => setCount(count = "VKontakte")} onMouseOut={() => setCount(count ="Социальные сети")}><a href="https://vk.com/" target="_blank"  rel="noreferrer"><i className="fab fa-vk"> </i></a></button>
-                <button onMouseOver={() => setCount(count = "Telegram")} onMouseOut={() => setCount(count ="Социальные сети")}><a href="https://telegram.org/" target="_blank"  rel="noreferrer"><i className="fab fa-telegram"> </i></a></button>
-                <button onMouseOver={() => setCount(count = "Discord")} onMouseOut={() => setCount(count ="Социальные сети")}><a href="https://discord.com/" target="_blank"  rel="noreferrer"><i className="fab fa-discord"> </i></a></button>
-                <button onMouseOver={() => setCount(count = "WhatsApp")} onMouseOut={() => setCount(count ="Социальные сети")}><a href="https://web.whatsapp.com/" target="_blank"  rel="noreferrer"><i className="fab fa-whatsapp"> </i></a></button>
-                <button onMouseOver={() => setCount(count = "Viber")} onMouseOut={() => setCount(count ="Социальные сети")}><a href="https://www.viber.com/ru/" target="_blank"  rel="noreferrer"><i className="fab fa-viber"> </i></a></button>
+                <button onMouseOver={() => setCount(count = "VKontakte")} onMouseOut={() => setCount(count = network)}><a href="https://vk.com/" target="_blank"  rel="noreferrer"><i className="fab fa-vk"> </i></a></button>
+                <button onMouseOver={() => setCount(count = "Telegram")} onMouseOut={() => setCount(count = network)}><a href="https://telegram.org/" target="_blank"  rel="noreferrer"><i className="fab fa-telegram"> </i></a></button>
+                <button onMouseOver={() => setCount(count = "Discord")} onMouseOut={() => setCount(count = network)}><a href="https://discord.com/" target="_blank"  rel="noreferrer"><i className="fab fa-discord"> </i></a></button>
+                <button onMouseOver={() => setCount(count = "WhatsApp")} onMouseOut={() => setCount(count = network)}><a href="https://web.whatsapp.com/" target="_blank"  rel="noreferrer"><i className="fab fa-whatsapp"> </i></a></button>
+                <button onMouseOver={() => setCount(count = "Viber")} onMouseOut={() => setCount(count = network)}><a href="https://www.viber.com/ru/" target="_blank"  rel="noreferrer"><i className="fab fa-viber"> </i></a></button>
             </div>
 
             <div className="social-network__title">
