51cfe362cd73c2561fad9344ae3de3664ec16246
Andrusha <andrew.ivanow505@gmail.com>
2021-10-19 19:17:20 +0300

    up

--- server/src/hook/usePagination.js
+++ server/src/hook/usePagination.js
@@ -1,25 +1,25 @@
-import React, {useState} from 'react';
+import {useState} from 'react';
 
 export const usePagination = (perPageRecords, totalRecords) => {
 
-    const totalPages=Math.ceil(totalRecords/perPageRecords);
+    const totalPages = Math.ceil(totalRecords/perPageRecords);
 
-    const [startPageIndex,setStartPageIndex]=useState(0)
-    const [endPageIndex,setEndPageIndex]=useState(perPageRecords-1);
-    const [currentPage,setCurrentPage]=useState(1);
+    const [startPageIndex, setStartPageIndex] = useState(0)
+    const [endPageIndex, setEndPageIndex] = useState(perPageRecords-1);
+    const [currentPage, setCurrentPage] = useState(1);
 
-    const dispPage=(pageNo)=>{
+    const Page = (pageNo) => {
 
         setCurrentPage(pageNo);
 
-        let start_page_index=(perPageRecords*pageNo)-perPageRecords;
-        let end_page_index=(perPageRecords*pageNo)-1;
+        let start_page_index = (perPageRecords*pageNo) - perPageRecords;
+        let end_page_index = (perPageRecords * pageNo) - 1;
 
         setStartPageIndex(start_page_index);
 
         if(end_page_index>totalRecords)
         {
-            setEndPageIndex(totalRecords-1);
+            setEndPageIndex(totalRecords - 1);
         }
         else{
             setEndPageIndex(end_page_index);
@@ -27,5 +27,5 @@ export const usePagination = (perPageRecords, totalRecords) => {
 
     }
 
-    return [totalPages, startPageIndex, endPageIndex, currentPage, dispPage]
+    return [totalPages, startPageIndex, endPageIndex, currentPage, Page]
 }
