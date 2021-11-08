cb119550a98c2717622fd5ea045c50742a9d96e3
Andrusha <andrew.ivanow505@gmail.com>
2021-11-08 20:17:08 +0300

    up

--- server/src/hook/usePagination.js
+++ server/src/hook/usePagination.js
@@ -5,19 +5,19 @@ export const usePagination = (perPageRecords, totalRecords) => {
     const totalPages = Math.ceil(totalRecords/perPageRecords);
 
     const [startPageIndex, setStartPageIndex] = useState(0)
-    const [endPageIndex, setEndPageIndex] = useState(perPageRecords-1);
+    const [endPageIndex, setEndPageIndex] = useState(perPageRecords - 1);
     const [currentPage, setCurrentPage] = useState(1);
 
     const Page = (pageNo) => {
 
         setCurrentPage(pageNo);
 
-        let start_page_index = (perPageRecords*pageNo) - perPageRecords;
+        let start_page_index = (perPageRecords * pageNo) - perPageRecords;
         let end_page_index = (perPageRecords * pageNo) - 1;
 
         setStartPageIndex(start_page_index);
 
-        if(end_page_index>totalRecords)
+        if(end_page_index > totalRecords)
         {
             setEndPageIndex(totalRecords - 1);
         }
