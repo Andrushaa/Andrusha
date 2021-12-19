5e6fc84c6ddf1d471dbf9872044c0b1b8034c703
Andrusha <andrew.ivanow505@gmail.com>
2021-12-19 23:17:42 +0300

    add API

---
+++ main/src/Components/education/API/API.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+export const EducationArrAPI = () => {
+    const educationArr = [{
+            id: 1,
+            text: 'Институт прикладной математики и компьютерных наук.'
+        }, {
+            id: 2,
+            text: 'кафедра: "Прикладная математика и информатика"'
+        }, {
+            id: 3,
+            text: 'Диплом бакалавра - средний балл 4,02'
+        }, {
+            id: 4,
+            text: 'Диплом магистра - средний балл 4,91 (красный диплом)'
+        }
+    ]
+
+    return (
+        <>
+            {
+                educationArr.map(item => <p key={item.id}>
+                    {item.text}
+                </p>)
+            }
+        </>
+
+    )
+}
+
+export const SkillArrAPI = () => {
+    const SkillArr = [{
+            id: 1,
+            text: 'HTML'
+        }, {
+            id: 2,
+            text: 'CSS'
+        }, {
+            id: 3,
+            text: 'Javascript'
+        }, {
+            id: 4,
+            text: 'SASS'
+        }, {
+            id: 5,
+            text: 'React'
+        }, {
+            id: 6,
+            text: 'Selenium'
+        }, {
+            id: 7,
+            text: 'C#'
+        }
+    ]
+
+    return (
+        <>
+            {
+
+                SkillArr.map(item => <p key={item.id}>
+                    {item.text}
+                </p>)
+
+            }
+        </>
+    )
+}
--- main/src/Components/education/Education.jsx
+++ main/src/Components/education/Education.jsx
@@ -1,44 +1,6 @@
 import React from 'react';
 import './education.css';
-
-const educationArr = [{
-        id: 1,
-        text: 'Институт прикладной математики и компьютерных наук.'
-    }, {
-        id: 2,
-        text: 'кафедра: "Прикладная математика и информатика"'
-    }, {
-        id: 3,
-        text: 'Диплом бакалавра - средний балл 4,02'
-    }, {
-        id: 4,
-        text: 'Диплом магистра - средний балл 4,91 (красный диплом)'
-    }
-]
-
-const SkillArr = [{
-        id: 1,
-        text: 'HTML'
-    }, {
-        id: 2,
-        text: 'CSS'
-    }, {
-        id: 3,
-        text: 'Javascript'
-    }, {
-        id: 4,
-        text: 'SASS'
-    }, {
-        id: 5,
-        text: 'React'
-    }, {
-        id: 6,
-        text: 'Selenium'
-    }, {
-        id: 7,
-        text: 'C#'
-    }
-]
+import {EducationArrAPI, SkillArrAPI} from "./API/API";
 
 function Education() {
     return (
@@ -46,17 +8,13 @@ function Education() {
             <div className="education-item">
                 <i className="fas fa-graduation-cap"> </i>
                 {
-                    educationArr.map(item => <p key={item.id}>
-                        {item.text}
-                    </p>)
+                    <EducationArrAPI/>
                 }
             </div>
             <div className="education-item">
                 <i className="fas fa-graduation-cap"> </i>
                 {
-                    SkillArr.map(item => <p key={item.id}>
-                        {item.text}
-                    </p>)
+                    <SkillArrAPI/>
                 }
             </div>
         </div>
